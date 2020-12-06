0c6c145077da0c9d7eb63144a450eeb5c9de9ae6
franc90 <azarnowski1@gmail.com>
2020-12-06 08:05:09 +0100

    day06 - test

--- day06/day06.py
+++ day06/day06.py
@@ -18,6 +18,12 @@ def count_common_letters_in_groups(groups):
     return sum(map(lambda g: len(collect_common_letters_in_group(g)), groups))
 
 
+test = split_groups_into_letters(read_with_group_separator('test', '\n\n'))
+uniq_letters = count_uniq_letters_in_groups(test)
+common_letters = count_common_letters_in_groups(test)
+assert 11 == uniq_letters, f"11 != {uniq_letters}"
+assert 6 == common_letters, f"6 != {common_letters}"
+
 text = read_with_group_separator('input', '\n\n')
 groups_with_letters = split_groups_into_letters(text)
 print(f"uniq:   {count_uniq_letters_in_groups(groups_with_letters)}")
